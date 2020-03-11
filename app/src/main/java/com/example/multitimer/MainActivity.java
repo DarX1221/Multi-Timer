bc02e9106af47606d38918f8bc0d1fe58f0d1bba
DarX1221 <darx1221@gmail.com>
2020-03-11 01:26:19 +0100

    Revert "Stopwatch timer is full work", make Timer2
    -create timer
    -should work in background
    
    This reverts commit 59c37ebfef8bc2643abeaf95169712a9ad9277da.
    
    # Conflicts:
    #	app/src/main/java/com/example/multitimer/SettingsFragment.java
    #	app/src/main/java/com/example/multitimer/StopWatchActivity.java
    #	app/src/main/java/com/example/multitimer/StopWatchFragment.java

--- app/src/main/java/com/example/multitimer/MainActivity.java
+++ app/src/main/java/com/example/multitimer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        stopWatchActivityStart();
+
 
     }
 
@@ -19,9 +20,21 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, StopWatchActivity.class);
         startActivity(intent);
     }
+    void timerActivityStart(){
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
+    }
 
     public MainActivity getMainActivity(){
         return this;
+    }
+
+    public void addClickStopwatch(View view) {
+        stopWatchActivityStart();
+    }
+
+    public void addClickTimer(View view) {
+        timerActivityStart();
     }
 }
 
